814249701cd044d0acaaacf9a5e14b8d44bcbea1
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2026-08-19 20:42:31 +0200

    Telescopes 2

--- HomeServerPage/HomeServerPage.Client/Data/Astronomy/Telescopes/TelescopeEyepiece.cs
+++ HomeServerPage/HomeServerPage.Client/Data/Astronomy/Telescopes/TelescopeEyepiece.cs
@@ -5,5 +5,15 @@ public record TelescopeEyepiece(
     double FieldOfView,
     double BarrelDiameter)
 {
-    public int Id { get; set; }
+    public int Id { get; init; }
+
+    public TelescopeEyepiece(
+        int Id,
+        double FocalLength,
+        double FieldOfView,
+        double BarrelDiameter)
+        : this(FocalLength, FieldOfView, BarrelDiameter)
+    {
+        this.Id = Id;
+    }
 }
--- HomeServerPage/HomeServerPage/Data/Astronomy/AstronomyDbContext.cs
+++ HomeServerPage/HomeServerPage/Data/Astronomy/AstronomyDbContext.cs
@@ -1,18 +1,46 @@
-﻿using HomeServerPage.Data.Astronomy.Telescopes;
+﻿using HomeServerPage.Client.Data.Astronomy.Telescopes;
 using Microsoft.EntityFrameworkCore;
 
 namespace HomeServerPage.Data.Astronomy;
 
-public class AstronomyDbContext : DbContext
+public class AstronomyDbContext(DbContextOptions<AstronomyDbContext> options) : DbContext(options)
 {
-
-    public AstronomyDbContext(DbContextOptions<AstronomyDbContext> options) : base(options)
-    {
-        
-    }
-
     public DbSet<TelescopeItem> Telescopes { get; set; }
 
     public DbSet<TelescopeEyepiece> Eyepieces { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TelescopeItem>().HasData(
+            new TelescopeItem(
+                Id: 1,
+                Name: "Sky-Watcher BKMAK 127 OTAW",
+                Type: TelescopeType.MaksutovCassegrain,
+                Aperture: 127,
+                FocalLength: 1500,
+                ApertureSpeed: 11.8),
+            new TelescopeItem(
+                Id: 2,
+                Name: "Sky-Watcher Evostar 72ED 72/420 F6",
+                Type: TelescopeType.Refractor,
+                Aperture: 72,
+                FocalLength: 420,
+                ApertureSpeed: 5.8),
+            new TelescopeItem(
+                Id: 2,
+                Name: "Sky-Watcher BK 1309 130/900",
+                Type: TelescopeType.Newtonian,
+                Aperture: 130,
+                FocalLength: 900,
+                ApertureSpeed: 6.9));
+
+        modelBuilder.Entity<TelescopeEyepiece>().HasData(
+            new TelescopeEyepiece(
+                Id: 1,
+                FocalLength: 25,
+                FieldOfView: 52,
+                BarrelDiameter: 1.25));
+    }
 }
