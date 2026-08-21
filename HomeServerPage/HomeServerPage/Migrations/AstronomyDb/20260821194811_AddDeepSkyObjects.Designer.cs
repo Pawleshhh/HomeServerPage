79545e48c19ad7bed0328c4e52c02e4e6ef5fac2
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2026-08-21 21:53:58 +0200

    DeepSkyObjects set EF update

--- HomeServerPage/HomeServerPage/Migrations/AstronomyDb/20260821194811_AddDeepSkyObjects.Designer.cs
+++ HomeServerPage/HomeServerPage/Migrations/AstronomyDb/20260821194811_AddDeepSkyObjects.Designer.cs
@@ -2,6 +2,7 @@
 using HomeServerPage.Data.Astronomy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeServerPage.Migrations.AstronomyDb
 {
     [DbContext(typeof(AstronomyDbContext))]
-    partial class AstronomyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260821194811_AddDeepSkyObjects")]
+    partial class AddDeepSkyObjects
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.9");
